5fee02738d6443416d8851b52985f6224408ce36
joconolimits <joconolimits@gmail.com>
2015-08-29 15:47:33 +0200

    OrderBy scripts improvements

---
+++ SEDC.TicketingSystem/Scripts/userOrderBy.js
@@ -0,0 +1,19 @@
+﻿// This Script is used for the Ajax ordering of the tickets/Index page.
+$(function () {
+    $('select').on('change', function () {
+        var selectedVal = $('#filterBy option:selected').val();
+        var ordering = $('#ordering option:selected').val();
+        $.ajax({
+            url: '/Tickets/OrderBy?x=' + selectedVal + '&ord=' + ordering,
+            contentType: 'application/html; charset=utf-8',
+            type: 'GET',
+            dataType: 'html'
+        })
+        .success(function (result) {
+            $('#tickets').html(result);
+        })
+        .error(function (xhr, status) {
+            alert(status);
+        })
+    });
+});
